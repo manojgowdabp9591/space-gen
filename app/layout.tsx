8386d7791eee4a7d0d93f1d45189250d3a8552ac
manojgowdabp9591 <manojgowdabp43@gmail.com>
2026-01-28 17:36:32 +0530

    update all

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,7 @@ export const metadata: Metadata = {
     siteName: "Space Gen",
     images: [
       {
-        url: "/social-preview.jpg",
+        url: "/social-preview.png",
         width: 1200,
         height: 630,
         alt: "Space Gen Launch Vehicle",
